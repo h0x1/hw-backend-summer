3d91fa4b74f480d6175405aa373800275dc6a4f0
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-09-11 03:20:19 +0300

    Update accessor.py

--- app/store/quiz/accessor.py
+++ app/store/quiz/accessor.py
@@ -1,51 +1,113 @@
-from typing import Optional
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload, subqueryload, selectinload
 
 from app.base.base_accessor import BaseAccessor
-from app.quiz.models import Theme, Question, Answer
+from app.quiz.models import (
+    Answer,
+    Question,
+    Theme, ThemeModel, AnswerModel, QuestionModel,
+)
 
 
 class QuizAccessor(BaseAccessor):
     async def create_theme(self, title: str) -> Theme:
-        theme = Theme(id=self.app.database.next_theme_id, title=str(title))
-        self.app.database.themes.append(theme)
-        return theme
+        async with self.app.database.session.begin() as session:
+            new_theme = ThemeModel(title=title)
+            session.add(new_theme)
 
-    async def get_theme_by_title(self, title: str) -> Optional[Theme]:
-        for theme in self.app.database.themes:
-            if theme.title == title:
-                return theme
-        return None
+        return new_theme.to_dc()
 
-    async def get_theme_by_id(self, id_: int) -> Optional[Theme]:
-        for theme in self.app.database.themes:
-            if theme.id == id_:
-                return theme
-        return None
+    async def get_theme_by_title(self, title: str) -> Theme | None:
+        async with self.app.database.session() as session:
+            theme = (await session.execute(
+                select(ThemeModel)
+                .where(ThemeModel.title == title)
+            )).scalar()
+
+        if not theme:
+            return
+
+        return theme.to_dc()
+
+    async def get_theme_by_id(self, id_: int) -> Theme | None:
+        async with self.app.database.session() as session:
+            theme = (await session.execute(
+                select(ThemeModel)
+                .where(ThemeModel.id == id_)
+            )).scalar()
+
+        if not theme:
+            return
+
+        return theme.to_dc()
 
     async def list_themes(self) -> list[Theme]:
-        return self.app.database.themes.copy()
+        async with self.app.database.session() as session:
+            themes_models = (await session.execute(
+                select(ThemeModel)
+            )).scalars().all()
 
-    async def get_question_by_title(self, title: str) -> Optional[Question]:
-        for question in self.app.database.questions:
-            if question.title == title:
-                return question
-        return None
+            themes = [theme_model.to_dc() for theme_model in themes_models]
+        return themes
+
+    async def create_answers(self, question_id: int, answers: list[Answer]) -> list[Answer]:
+        async with self.app.database.session() as session:
+            new_answers = [
+                AnswerModel(
+                    title=answer.title,
+                    is_correct=answer.is_correct,
+                    question_id=question_id
+                ) for answer in answers
+            ]
+            session.add_all(new_answers)
+            await session.commit()
+
+            answers = [answer.to_dc() for answer in new_answers]
+            return answers
 
     async def create_question(self, title: str, theme_id: int, answers: list[Answer]) -> Question:
-        question = Question(
-            title=title,
-            theme_id=theme_id,
-            answers=answers,
-            id=self.app.database.next_question_id
-        )
+        async with self.app.database.session() as session:
+            answer_models = [
+                AnswerModel(
+                    title=answer.title,
+                    is_correct=answer.is_correct
+                ) for answer in answers
+            ]
+            new_question = QuestionModel(
+                title=title,
+                theme_id=theme_id,
+                answers=answer_models)
 
-        self.app.database.questions.append(question)
-        return question
+            session.add(new_question)
+            await session.commit()
 
-    async def list_questions(self, theme_id: Optional[int] = None) -> list[Question]:
-        all_questions = self.app.database.questions
+        return new_question.to_dc()
 
-        if theme_id is None:
-            return all_questions
+    async def get_question_by_title(self, title: str) -> Question | None:
+        async with self.app.database.session() as session:
+            question = (await session.execute(
+                select(QuestionModel)
+                .where(QuestionModel.title == title)
+                .options(joinedload(QuestionModel.answers))
+            )).scalar()
 
-        return list(filter(lambda q: q.theme_id == theme_id, all_questions))
+        if not question:
+            return
+
+        return question.to_dc()
+
+    async def list_questions(self, theme_id: int | None = None) -> list[Question]:
+        async with self.app.database.session() as session:
+            query = select(QuestionModel)
+
+            if theme_id:
+                query = query.where(QuestionModel.theme_id == theme_id)
+
+            question_models = (await session.execute(
+                query.options(joinedload(QuestionModel.answers))
+            )).scalars().unique().all()
+
+            questions = [question_model.to_dc() for question_model in question_models]
+
+        return questions
+
