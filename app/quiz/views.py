8352561b5a87c200cf80a6e5d99787cfd9ba5a38
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-09-11 13:34:45 +0300

    Update views.py

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,70 +1,74 @@
-from aiohttp.web_exceptions import HTTPConflict, HTTPNotFound
-from aiohttp_apispec import docs, request_schema, response_schema, querystring_schema
+from aiohttp.web_exceptions import HTTPConflict, HTTPNotFound, HTTPBadRequest
+from aiohttp_apispec import querystring_schema, request_schema, response_schema, json_schema
 
-from app.auth.decorators import auth_required
+from app.quiz.models import Answer
 from app.quiz.schemes import (
-    ThemeRequestSchema, ThemeListResponseSchema, QuestionRequestSchema, QuestionResponseSchema,
-    ThemeResponseSchema, QuestionListResponseSchema, QuestionRequestQuerySchema,
-)
+    ListQuestionSchema, QuestionSchema, 
+    ThemeIdSchema, ThemeListSchema, ThemeSchema)
 from app.web.app import View
+from app.web.mixins import AuthRequiredMixin
 from app.web.utils import json_response
 
 
-class ThemeAddView(View):
-    @docs(tags=["quiz"], summary="Add new theme", description="Add new theme to database")
-    @request_schema(ThemeRequestSchema)
-    @response_schema(ThemeResponseSchema, 200)
-    @auth_required
+class ThemeAddView(AuthRequiredMixin, View):
+    @request_schema(ThemeSchema)
+    @response_schema(ThemeSchema)
     async def post(self):
         title = self.data["title"]
+        exist_theme = await self.store.quizzes.get_theme_by_title(title)
 
-        if await self.store.quizzes.get_theme_by_title(title=title) is not None:
+        if exist_theme:
             raise HTTPConflict
 
         theme = await self.store.quizzes.create_theme(title=title)
-        return json_response(data=ThemeResponseSchema().dump(theme))
+        return json_response(data=ThemeSchema().dump(theme))
 
 
-class ThemeListView(View):
-    @docs(tags=["quiz"], summary="Get list of themes", description="Get list all of themes from database")
-    @response_schema(ThemeListResponseSchema, 200)
-    @auth_required
+class ThemeListView(AuthRequiredMixin, View):
+    @response_schema(ThemeListSchema)
     async def get(self):
-        themes = await self.store.quizzes.list_themes()
-        return json_response(data={'themes': ThemeResponseSchema().dump(themes, many=True)})
+        list_themes = await self.store.quizzes.list_themes()
+        return json_response(data=ThemeListSchema().dump({"themes": list_themes}))
 
 
-class QuestionAddView(View):
-    @docs(tags=["quiz"], summary="Add new question", description="Add new question to database")
-    @request_schema(QuestionRequestSchema)
-    @response_schema(QuestionResponseSchema, 200)
-    @auth_required
+class QuestionAddView(AuthRequiredMixin, View):
+    @request_schema(QuestionSchema)
+    @response_schema(QuestionSchema)
     async def post(self):
-        title, theme_id, answers = self.data['title'], self.data['theme_id'], self.data['answers']
+        theme_id = self.data["theme_id"]
+        title = self.data["title"]
+        answers = self.data["answers"]
 
-        if await self.store.quizzes.get_question_by_title(title) is not None:
-            raise HTTPConflict
-
-        if await self.store.quizzes.get_theme_by_id(theme_id) is None:
+        theme = await self.store.quizzes.get_theme_by_id(theme_id)
+        if not theme:
             raise HTTPNotFound
 
+        existed_question = await self.store.quizzes.get_question_by_title(title)
+        if existed_question:
+            raise HTTPConflict
+
+        total_correct_answers = sum(answer["is_correct"] for answer in answers)
+        if total_correct_answers != 1 or len(self.data['answers']) <= 1:
+            raise HTTPBadRequest
+
         question = await self.store.quizzes.create_question(
-            title,
-            theme_id,
-            answers
+            title=title,
+            theme_id=theme_id,
+            answers=[
+                Answer(
+                    title=answer["title"],
+                    is_correct=answer["is_correct"]
+                ) for answer in answers
+            ]
         )
 
-        return json_response(data=QuestionResponseSchema().dump(question))
+        return json_response(data=QuestionSchema().dump(question))
 
 
-class QuestionListView(View):
-    @docs(tags=["quiz"], summary="Get list of questions", description="Get list all of questions from database")
-    @querystring_schema(QuestionRequestQuerySchema)
-    @response_schema(QuestionListResponseSchema, 200)
-    @auth_required
+class QuestionListView(AuthRequiredMixin, View):
+    @querystring_schema(ThemeIdSchema)
+    @response_schema(ListQuestionSchema)
     async def get(self):
-        theme_id = self.request.query.get('theme_id')
-        questions = await self.store.quizzes.list_questions(theme_id)
-        return json_response(data={'questions': QuestionResponseSchema().dump(questions, many=True)})
-
-
+        theme_id = self.request.query.get("theme_id")
+        questions = await self.store.quizzes.list_questions(theme_id=theme_id)
+        return json_response(data=ListQuestionSchema().dump({"questions": questions}))
