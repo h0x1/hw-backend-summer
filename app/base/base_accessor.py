24282d9effc8f28b610095aea63e3beb03a04911
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-09-11 13:11:36 +0300

    Update base_accessor.py

--- app/base/base_accessor.py
+++ app/base/base_accessor.py
@@ -1,14 +1,11 @@
 import typing
-from logging import getLogger
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
 
-
 class BaseAccessor:
     def __init__(self, app: "Application", *args, **kwargs):
         self.app = app
-        self.logger = getLogger("accessor")
         app.on_startup.append(self.connect)
         app.on_cleanup.append(self.disconnect)
 
