ddbeb6746b1613c791cd4972d224a524789deb11
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-09-11 12:56:00 +0300

    Update accessor.py

--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -1,16 +1,18 @@
+import random
 import typing
-from pprint import pprint
 from typing import Optional
 
-import aiohttp
+from aiohttp import TCPConnector
 from aiohttp.client import ClientSession
 
 from app.base.base_accessor import BaseAccessor
-from app.store.vk_api.dataclasses import Message, Update, UpdateObject, UpdateMessage
+from app.store.vk_api.dataclasses import Message, Update, UpdateObject
 from app.store.vk_api.poller import Poller
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
+
+API_PATH = "https://api.vk.com/method/"
 
 
 class VkApiAccessor(BaseAccessor):
@@ -23,19 +25,20 @@ class VkApiAccessor(BaseAccessor):
         self.ts: Optional[int] = None
 
     async def connect(self, app: "Application"):
-        self.session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
-        self.poller = Poller(self.app.store)
-        await self._get_long_poll_service()
+        self.session = ClientSession(connector=TCPConnector(verify_ssl=False))
+        try:
+            await self._get_long_poll_service()
+        except Exception as e:
+            self.logger.error("Exception", exc_info=e)
+        self.poller = Poller(app.store)
+        self.logger.info("start polling")
         await self.poller.start()
 
     async def disconnect(self, app: "Application"):
-        if self.session is not None:
+        if self.session:
             await self.session.close()
-            self.session = None
-
-        if self.poller is not None and self.poller.is_running:
+        if self.poller:
             await self.poller.stop()
-            self.poller = None
 
     @staticmethod
     def _build_query(host: str, method: str, params: dict) -> str:
@@ -46,64 +49,67 @@ class VkApiAccessor(BaseAccessor):
         return url
 
     async def _get_long_poll_service(self):
-        group_id = self.app.config.bot.group_id
-        token = self.app.config.bot.token
+        async with self.session.get(
+            self._build_query(
+                host=API_PATH,
+                method="groups.getLongPollServer",
+                params={
+                    "group_id": self.app.config.bot.group_id,
+                    "access_token": self.app.config.bot.token,
+                },
+            )
+        ) as resp:
+            data = (await resp.json())["response"]
+            self.logger.info(data)
+            self.key = data["key"]
+            self.server = data["server"]
+            self.ts = data["ts"]
+            self.logger.info(self.server)
 
-        query = self._build_query(
-            host='https://api.vk.com/',
-            method='method/groups.getLongPollServer',
-
-            params={'group_id': group_id, 'access_token': token}
-        )
-
-        async with self.session.get(query) as response:
-            response_body = (await response.json())['response']
-            self.key = response_body['key']
-            self.server = response_body['server']
-            self.ts = response_body['ts']
-
-    async def poll(self) -> list[Update]:
-        query = self._build_query(
-            host=self.server,
-            method='',
-            params={
-                'act': 'a_check',
-                'key': self.key,
-                'wait': 25,
-                'mode': 2,
-                'ts': self.ts
-            })
-
-        async with self.session.get(query) as response:
-            resp_json: dict = await response.json()
-            self.ts = resp_json['ts']
-            raw_updates = resp_json['updates']
-
-        return self._pack_updates(raw_updates)
+    async def poll(self):
+        async with self.session.get(
+            self._build_query(
+                host=self.server,
+                method="",
+                params={
+                    "act": "a_check",
+                    "key": self.key,
+                    "ts": self.ts,
+                    "wait": 30,
+                },
+            )
+        ) as resp:
+            data = await resp.json()
+            self.logger.info(data)
+            self.ts = data["ts"]
+            raw_updates = data.get("updates", [])
+            updates = []
+            for update in raw_updates:
+                updates.append(
+                    Update(
+                        type=update["type"],
+                        object=UpdateObject(
+                            id=update["object"]["id"],
+                            user_id=update["object"]["user_id"],
+                            body=update["object"]["body"],
+                        ),
+                    )
+                )
+            await self.app.store.bots_manager.handle_updates(updates)
 
     async def send_message(self, message: Message) -> None:
-        query = self._build_query(
-            host='https://api.vk.com/',
-            method='method/messages.send',
-            params={'message': message.text,
-                    'access_token': self.app.config.bot.token,
-                    'group_id': self.app.config.bot.group_id,
-                    'random_id': 0,
-                    'peer_id': -202369435,
-                    'user_id': message.user_id,
-                    }
-        )
-
-        async with self.session.get(query) as resp:
-            pprint(await resp.json())
-
-    @staticmethod
-    def _pack_updates(raw_updates: dict) -> list[Update]:
-        return [
-            Update(
-                type=u['type'],
-                object=UpdateObject(
-                    message=UpdateMessage(
-                        from_id=u['object']['message']['from_id'],
-                        text=u['object']['message']['text'],
-                        id=u['object']['message']['id']))) for u in raw_updates if u['type'] == 'message_new']
+        async with self.session.get(
+            self._build_query(
+                API_PATH,
+                "messages.send",
+                params={
+                    "user_id": message.user_id,
+                    "random_id": random.randint(1, 2**32),
+                    "peer_id": "-" + str(self.app.config.bot.group_id),
+                    "message": message.text,
+                    "access_token": self.app.config.bot.token,
+                },
+            )
+        ) as resp:
+            data = await resp.json()
+            self.logger.info(data)
